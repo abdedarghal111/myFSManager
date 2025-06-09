5f3eaa3cfa8bc0b7556f5201abce34804b73d0e8
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2025-06-09 11:11:59 +0200

    chore: save changes

--- myFSManager.fish
+++ myFSManager.fish
@@ -26,6 +26,7 @@ if not test -d $DOCKERS_DIR
     echo "Directorio 'dockers' creado"
 end
 
+set IMAGES "php:8.4-cli php:8.2-cli php:8.0-cli php:7.4-cli php:7.2-cli php:7.0-cli adminer mysql:latest"
 
 
 switch $argv[1]
@@ -33,7 +34,7 @@ switch $argv[1]
     # Rango de versiones PHP
     set PHP_VERSIONS 84 82 80 74 72 70
 
-    # Generar Dockerfiles y construir imágenes
+    # Generar Dockerfiles y construir imágenes de php
     for ver in $PHP_VERSIONS
         set TAG (string sub -s 1 -l 1 $ver).(string sub -s 2 -l 1 $ver)
         set NAME "php$ver"
@@ -41,19 +42,31 @@ switch $argv[1]
 
         echo "📝 Generando Dockerfile para PHP $TAG -> $FILE"
 
-        echo "FROM php:$TAG-cli
-
-          RUN apt-get update && apt-get install -y \\
-              default-mysql-client \\
-              && docker-php-ext-install pdo pdo_mysql
-
-          WORKDIR /var/www
-        " > $FILE
-          
+        echo "FROM php:$TAG-cli" > $FILE
 
         echo "🐳 Construyendo imagen $NAME..."
         docker build -f $FILE -t $NAME .
     end
+
+    # Generar Dockerfile y construir imágen de adminer
+    set ADMINER_FILE "$DOCKERS_DIR/adminer.dockerfile"
+
+    echo "📝 Generando Dockerfile para Adminer -> $ADMINER_FILE"
+    echo "FROM adminer" > $ADMINER_FILE
+    echo "EXPOSE 8080" >> $ADMINER_FILE
+
+    echo "🐳 Construyendo imagen adminer..."
+    docker build -f $ADMINER_FILE -t adminer .
+
+    # Generar Dockerfile y construir imágen de mysql
+
+    set MYSQL_FILE "$DOCKERS_DIR/mysql.dockerfile"
+
+    echo "📝 Generando Dockerfile para MySQL -> $MYSQL_FILE"
+    echo "FROM mysql:latest" > $MYSQL_FILE
+    echo "ENV MYSQL_ROOT_PASSWORD=root" >> $MYSQL_FILE
+    echo "ENV MYSQL_DATABASE=facturascripts" >> $MYSQL_FILE
+    echo "EXPOSE 3306" >> $MYSQL_FILE
 
     echo "✅ Todo listo. Dockerfiles y imágenes generados."  
 end
